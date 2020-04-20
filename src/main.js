81f1a19f3ef3dee42b9bab70b7790eb939943027
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-20 09:17:56 +0200

    Add Ionic

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/ionic.js'
+import Ionic from '@ionic/vue';
+
 
 Vue.config.productionTip = false
+Vue.use(Ionic)
+
+let bus = new Vue
+Vue.prototype.$bus = bus
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content padding>
+      <ion-list>
+        <ion-item>
+          <ion-checkbox slot="start"></ion-checkbox>
+          <ion-label>
+            <h1>Create Idea</h1>
+            <ion-note>Run Idea by Brandy</ion-note>
+          </ion-label>
+          <ion-badge color="success" slot="end">5 Days</ion-badge>
+        </ion-item>
+      </ion-list>
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button >
+          <ion-icon name="add" />
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
